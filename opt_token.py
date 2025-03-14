72b084aea287bd31fe2af664d026e461a2548f16
Pyoung Ku Kim <pyoungkukim@gmail.com>
2025-03-13 23:03:22 -0400

    still testing cloud buils

--- opt_token.py
+++ opt_token.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 
 # Basic infos (You might want to set these via environment variables)
 auth_type = 'totp'
-#testing cloud build CI/CD
+#testing cloud build CI/CD111
 
 # Setting Google Storage services instances
 storage_client = storage.Client()
